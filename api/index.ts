032e63e6a37516ee4adc509f22c8c2b30fc78217
Maksim Tkashev <maks93makula@gmail.com>
2024-04-06 12:05:16 +0300

    Инициализация проект backend

---
+++ api/index.ts
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 8000;
+
+app.use(cors())
+app.use(express.json());
+
+app.use(express.static('public/images'));
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+});
